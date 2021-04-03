3fabc8ae5de445c26a3d79f4045ac59eb298ff76
Arsh-ak7 <arshkr07@gmail.com>
2021-04-03 12:29:20 +0530

    Added Internship

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import '../css/projects.css';
-import NetflixImage from '../images/Netflix.jpg';
-import AirbnbImage from '../images/airbnb.png';
-import YoutubeImage from '../images/youtube.jpg';
-import GalleryImage from '../images/gallery.jpg';
-import Media from '../images/mm.png';
-import WeatherImage from '../images/weather.jpg';
-import MessmanagementImage from '../images/messmanagement.png';
+import React from "react";
+import "../css/projects.css";
+import NetflixImage from "../images/Netflix.jpg";
+import AirbnbImage from "../images/airbnb.png";
+import YoutubeImage from "../images/youtube.jpg";
+import GalleryImage from "../images/gallery.jpg";
+import Media from "../images/mm.png";
+import WeatherImage from "../images/weather.jpg";
+import MessmanagementImage from "../images/messmanagement.png";
 
 export default function Projects() {
 	return (
 		<div className='projects' id='projects'>
+			<div className='internship'>
+				<h1 className='banner-head'>INTERNSHIP</h1>
+				<h1 className='internship-head'>
+					Web Development Intern at DETERMINANT STUDIOS
+				</h1>
+
+				<h1 className='internship-head'>Jan 2021 - March 2021</h1>
+			</div>
 			<div className='banner'>
-				<h1 className='banner-head'> My PROJECTS</h1>
+				<h1 className='banner-head'> SELF MADE PROJECTS</h1>
 			</div>
 			<div className='row'>
 				<div className='row-posters'>
@@ -20,8 +28,7 @@ export default function Projects() {
 						<a
 							href='https://secret-coast-92182.herokuapp.com/'
 							target='_blank'
-							rel='noreferrer'
-						>
+							rel='noreferrer'>
 							<img
 								src={MessmanagementImage}
 								alt='img'
@@ -37,8 +44,7 @@ export default function Projects() {
 						<a
 							href='https://netflix-clone-d3d95.web.app/'
 							target='_blank'
-							rel='noreferrer'
-						>
+							rel='noreferrer'>
 							<img
 								src={NetflixImage}
 								alt='img'
@@ -55,8 +61,7 @@ export default function Projects() {
 						<a
 							href='https://airbnb-clone-f74de.web.app/'
 							target='_blank'
-							rel='noreferrer'
-						>
+							rel='noreferrer'>
 							<img src={AirbnbImage} alt='img' className='image-tile airbnb' />
 						</a>
 						<span className='subheading'>AIRBNB CLONE</span>
@@ -66,8 +71,7 @@ export default function Projects() {
 						<a
 							href='https://sampletwi.netlify.app/'
 							target='_blank'
-							rel='noreferrer'
-						>
+							rel='noreferrer'>
 							<img src={Media} alt='img' className='image-tile media' />
 						</a>
 						<span className='subheading'>BASIC SOCIAL MEDIA APP</span>
@@ -80,8 +84,7 @@ export default function Projects() {
 						<a
 							href='https://arsh-gallery.netlify.app/'
 							target='_blank'
-							rel='noreferrer'
-						>
+							rel='noreferrer'>
 							<img
 								src={GalleryImage}
 								alt='img'
@@ -98,8 +101,7 @@ export default function Projects() {
 						<a
 							href='https://clo-63a81.web.app/'
 							target='_blank'
-							rel='noreferrer'
-						>
+							rel='noreferrer'>
 							<img src={YoutubeImage} alt='img' className='image-tile ytb' />
 						</a>
 						<span className='subheading'>YOUTUBE CLONE</span>
@@ -109,8 +111,7 @@ export default function Projects() {
 						<a
 							href='https://arsh-weatherapp.netlify.app/'
 							target='_blank'
-							rel='noreferrer'
-						>
+							rel='noreferrer'>
 							<img
 								src={WeatherImage}
 								alt='img'
