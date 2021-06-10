941fcfe325f7fd0f0831837229794c4efe621f3b
Arsh-ak7 <arshkr07@gmail.com>
2021-06-10 12:27:23 +0530

    Started Mobile view optimizations

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import '../css/home.css';
+import React, { useEffect, useState } from "react";
+import "../css/home.css";
 
 export default function Home() {
 	const [scrolled, setScrolled] = useState(false);
@@ -11,25 +11,25 @@ export default function Home() {
 		}
 	};
 	useEffect(() => {
-		window.addEventListener('scroll', handleScroll, { passive: true });
+		window.addEventListener("scroll", handleScroll, { passive: true });
 
 		return () => {
-			window.removeEventListener('scroll', handleScroll);
+			window.removeEventListener("scroll", handleScroll);
 		};
 	}, []);
 	return (
 		<div className='home' id='home'>
-			<h1 className={`${scrolled ? 'scrolled header ml11' : 'header ml11'}`}>
+			<h1 className={`${scrolled ? "scrolled header ml11" : "header ml11"}`}>
 				<span className='text-wrapper'>
+					<h4 className='intro-start'>I'm a</h4>
 					<span className='line line1'></span>
 					<span className='letters'>WEB DEVELOPER</span>
 				</span>
 			</h1>
 			<div
 				className={`${
-					scrolled ? 'scrolled scroll-wording' : 'scroll-wording'
-				} `}
-			>
+					scrolled ? "scrolled scroll-wording" : "scroll-wording"
+				} `}>
 				<span className='scroll-word'>Scroll &gt;</span>
 			</div>
 		</div>
